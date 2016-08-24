c22d158347353d95b714abe8886901e03b3c6074
zouzhenxing <zouzx@yunku.org>
2016-08-24 16:15:00 +0800

    '增加文件上传'

--- util.js
+++ util.js
@@ -58,6 +58,29 @@ var sqlFormat = function (query, values) {
     }.bind(this));
 };
 
+
+//配置文件上传
+var multer = require('multer');
+exports.upfile = () => {
+    var storage = multer.diskStorage({
+        //设置上传后文件路径，uploads文件夹会自动创建。
+        destination: function (req, file, cb) {
+            cb(null, './public/uploads')
+        }, 
+        //给上传文件重命名，获取添加后缀名
+        filename: function (req, file, cb) {
+            var fileFormat = (file.originalname).split(".");
+            cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+        }
+    });
+
+    return multer({
+        storage: storage,
+        limits:{}
+    });
+}
+
+
 //处理favicon请求
 exports.icoHandle = (req,res,next)=> {
     res.sendFile(rootPath.concat("/public/favicon.ico"));
